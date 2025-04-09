1616a33d40fc8e6e8815dc3a56a4ce8de6a35561
TemirovaAzima <temirovaazima9@gmail.com>
2025-04-09 14:02:45 +0500

    new Map and map set has its own generics that appears automatically

--- index.ts
+++ index.ts
@@ -1,38 +1,7 @@
-// function getFirstElement(array:number[]){
-//     return array[0]
-// }
-//
-// const numbers = [1,2,3];
-// const firstNum =getFirstElement(numbers);
-//
-// const strings = ['hehe','hjv'];
-// const firstString =getFirstElement(strings);
-// function getFirstELement(array:any[]){
-//     return array[0]
-// }
-//
-// const numbers = [1,2,3];
-// const firstNum =getFirstELement(numbers);
-//
-// const strings = ['hehe','hjv'];
-// const firstString = getFirstELement(strings);
-// function getFirstELement(array:(number|string)[]){
-//     return array[0]
-// }
-//
-// const numbers = [1,2,3];
-// const firstNum =getFirstELement(numbers);
-//
-// const strings = ['hehe','hjv'];
-// const firstString = getFirstELement(strings);
-// has some type of data inside of the function where u want that data to be changing its type based on what u pass in
-// or based on what u return that is the perfect case for a generic
-function getFirstELement<ElementType>(array:ElementType[]){
-    return array[0]
-}
 
-const numbers = [1,2,3];
-const firstNum =getFirstELement<number>(numbers);
+const map = new Map<string,number>()
+map.set('sdf',2)
 
-const strings = ['hehe','hjv'];
-const firstString = getFirstELement(strings);
+
+const map2 = new Map([['sdf',2]])
+map2.set('sdf',2)
